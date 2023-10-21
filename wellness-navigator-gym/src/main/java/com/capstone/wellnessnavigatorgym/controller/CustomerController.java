5b4ce19e09bca62aff0013fb94e157e413f73f8d
tienthanh11 <dotienthanh1121@gmail.com>
2023-10-21 16:16:45 +0700

    dev

--- wellness-navigator-gym/src/main/java/com/capstone/wellnessnavigatorgym/controller/CustomerController.java
+++ wellness-navigator-gym/src/main/java/com/capstone/wellnessnavigatorgym/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.capstone.wellnessnavigatorgym.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerController controller;
 }
