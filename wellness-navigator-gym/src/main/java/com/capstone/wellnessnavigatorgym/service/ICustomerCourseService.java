f31dc1fa256f498c552f9c00c6dcea5c73b0f87c
tienthanh11 <dotienthanh1121@gmail.com>
2023-12-14 20:32:58 +0700

    add

--- wellness-navigator-gym/src/main/java/com/capstone/wellnessnavigatorgym/service/ICustomerCourseService.java
+++ wellness-navigator-gym/src/main/java/com/capstone/wellnessnavigatorgym/service/ICustomerCourseService.java
@@ -1,4 +1,4 @@
 package com.capstone.wellnessnavigatorgym.service;
 
-public interface ICourseDayService {
+public interface ICustomerCourseService {
 }
---
+++ wellness-navigator-gym/src/main/java/com/capstone/wellnessnavigatorgym/service/impl/CustomerCourseServiceImpl.java
@@ -0,0 +1,8 @@
+package com.capstone.wellnessnavigatorgym.service.impl;
+
+import com.capstone.wellnessnavigatorgym.service.ICustomerCourseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerCourseServiceImpl implements ICustomerCourseService {
+}
