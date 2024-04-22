562cb283b8aa0aeab8dc715e555500f373a6b8de
tienthanh11 <dotienthanh1121@gmail.com>
2024-04-22 22:50:22 +0700

    update

--- wellness-navigator-gym/src/main/java/com/capstone/wellnessnavigatorgym/controller/CourseController.java
+++ wellness-navigator-gym/src/main/java/com/capstone/wellnessnavigatorgym/controller/CourseController.java
@@ -8,8 +8,6 @@ import com.capstone.wellnessnavigatorgym.service.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
